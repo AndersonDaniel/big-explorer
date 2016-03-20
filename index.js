3c3a8e0cba241c26e42129c637736c88d3adcf59
AndersonDaniel <Daniel.Anderson.D@gmail.com>
2016-03-20 05:41:46 +0200

    Some more stuff

--- index.js
+++ index.js
@@ -3,6 +3,15 @@ angular.module('explorer', []).
 		var self = this;
 		self.bla = 'shtut';
 		
+		self.open = function(file) {
+			$http.post('/open/' + file['name'] + '.' + file['type']);
+			console.log('Opened ', file);
+		};
+		
+		self.newfile = function() {
+			alert('creating new file');
+		};
+		
 		$http.get('/files').then(function(data) {
 			self.data = data.data;
 		});
--- server.py
+++ server.py
@@ -2,16 +2,25 @@ import http.server
 import socketserver
 import json
 import os
+import urllib.parse
 
 CONST_WORKING_FOLDER = r'D:\daniel\working_folder'
 
 class myHTTPRequestHandler(http.server.SimpleHTTPRequestHandler):
 	def do_GET(self):
 		if (self.path == '/files'):
-			self.wfile.write(json.dumps(os.listdir(CONST_WORKING_FOLDER)).encode())
-			#self.wfile.write(json.dumps(['אבג']).encode())
+			data = [{'name': file.split('.')[0], 'type': file.split('.')[1]} for file in os.listdir(CONST_WORKING_FOLDER) if file != 'exec.bat']
+			self.wfile.write(json.dumps(data).encode())
 		else:
 			super(myHTTPRequestHandler, self).do_GET()
+			
+	def do_POST(self):
+		if (self.path.startswith('/open/')):
+			file = urllib.parse.unquote(self.path.replace('/open/', ''))
+			os.system('start "ook" "' + CONST_WORKING_FOLDER + '\\' + file + '"')
+			self.wfile.write('ok'.encode())
+		else:
+			super(myHTTPRequestHandler, self).do_POST()
 
 def serve():
 	PORT = 8000
